c728e7b52b0c7f5d112c2a60334367983e809d36
Yamini Arunachalam <yaminia@amazon.com>
2017-11-05 13:45:09 +0530

    Commit

--- src/test/java/TestEmailNotifier.java
+++ src/test/java/TestEmailNotifier.java
@@ -4,22 +4,35 @@ import techexe.expedia.notifier.EmailNotifier;
 
 import java.util.List;
 
+/**
+ * Test email notifier contains tests to verify
+ * email notifications sent to users who have voted for popular places
+ */
 public class TestEmailNotifier {
     private EmailNotifier notifier;
 
+    /**
+     * Instantiates a new Test email notifier.
+     */
     public TestEmailNotifier(){
         notifier= new EmailNotifier();
     }
 
 
+    /**
+     * Test email notification.
+     */
     @Test(description = "Verify if email notification is being sent",enabled = false)
     public void testEmailNotification(){
         notifier.sendEmailNotification();
     }
 
+    /**
+     * Test list of email ids to send notofications.
+     */
     @Test(description = "Verify the no of emails Ids to be notified")
     public void testListOfEmailIds(){
         List<String> emailIds=notifier.getListofUserEmailIds();
-        Assert.assertEquals(emailIds.size(),2,"EmailIds Coutn doesnot match");
+        Assert.assertEquals(emailIds.size(),2,"EmailIds Count doesnot match");
     }
 }
